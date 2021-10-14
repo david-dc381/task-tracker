cfeb8c7c85d8fe5ed1a92f7a4cc8d0d5cea1f1ea
David Cayo <david_dc381@hotmail.com>
2021-10-13 22:25:19 -0400

    Se añadio estilos a los botones

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ const Header = ({ title, onAdd , showAdd}) => {
         /* onAdd, lo usamos para mostrar el form de registro */
         <Button
           buttonOpenClose={onAdd}
-          color={showAdd ? "#fb2d2d" : "#05bf05"}
+          className={showAdd ? "btn btn-danger" : "btn btn-success"}
           text={showAdd ? "Close" : "Add Task"}
         />
       )}
